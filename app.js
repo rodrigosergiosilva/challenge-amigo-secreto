d49188ebe9d5a540de1af616b96d43ec955310bf
Rodrigo Sergio <rsergios@yahoo.com.br>
2025-02-04 10:26:41 -0300

    Implementa uma função para sortear os amigos

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ let amigos = [];
 
 function adicionarAmigo(){
     let amigo = document.querySelector("input");
-    if (amigo.value == '') {
+    let resultado = document.getElementById("resultado");   
+    resultado.innerHTML = '';
+
+    if (amigo.value === '') {
         alert("Por favor, insira um nome.");
     } else{
         amigos.push(amigo.value);
         amigo.value = '';
     }    
     atualizarListaAmigos();
+    
 }
 
 function atualizarListaAmigos(){
@@ -19,5 +23,19 @@ function atualizarListaAmigos(){
         let elementoLista = document.createElement("li");
         elementoLista.textContent = amigos[i];
         lista.appendChild(elementoLista) ;
+    }
+}
+
+function sortearAmigo() {
+    if ((amigos.length) > 1){
+        let numeroLimite = amigos.length;
+        let indiceAleatorio = parseInt(Math.floor(Math.random() * numeroLimite));
+        let lista = document.getElementById("listaAmigos");    
+        lista.innerHTML = "";        
+        let resultado = document.getElementById("resultado");   
+        resultado.innerHTML = `O amigo secreto sorteado é: ${amigos[indiceAleatorio]}`;
+        amigos = [];
+    } else {
+        alert("A lista de amigos deve ter no mínimo 2 nomes para realizar o sorteio.")
     }
 }
